1bd403b102f7e314777b2a5abfcb4c0a87ceb8fa
harumi <akiharu46@gmail.com>
2017-12-13 11:21:56 +0900

    NPCスクリプト

---
+++ PhotonNDKGame/Assets/Photon Unity Networking/Resources/Script/NPC/NpcEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class NpcEvent : MonoBehaviour {
+  
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Playertagを持っているものが当たると
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("おんどぅる");
+            //Destroy(gameObject);
+        }
+    }
+
+}
