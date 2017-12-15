9a3ac432fd402f7ffa74dd157028eb6847bb8fcf
doi <a>
2017-12-15 10:14:01 +0900

    Title編集。doorScript追加

---
+++ PhotonNDKGame/Assets/Photon Unity Networking/Resources/Script/door/door.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class door : MonoBehaviour
+{
+    void OnTriggerStay(Collider collider)
+    {
+        if (Input.GetKey(KeyCode.Joystick1Button0))
+        {
+            if (collider.gameObject.tag == "Player")
+            {
+
+                collider.gameObject.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+                //SceneManager.LoadScene("DoiScene2");
+            }
+        }
+    }
+}
