05a8855821dcaee928dcfd5d5b0c68ed6478f9cb
harumi <akiharu46@gmail.com>
2018-02-01 10:38:04 +0900

    SoundManagerのエラー直した

--- PhotonNDKGame/Assets/Photon Unity Networking/Resources/Script/Haru/SoundManager.cs
+++ PhotonNDKGame/Assets/Photon Unity Networking/Resources/Script/Haru/SoundManager.cs
@@ -1,11 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+//音管理クラス
+[Serializable]
+public class SoundVolume
+{
+    public float BGM = 1.0f;
+    public float Voice = 1.0f;
+    public float SE = 1.0f;
+    public bool Mute = false;
+    public void Init()
+    {
+        BGM = 1.0f;
+        Voice = 1.0f;
+        SE = 1.0f;
+        Mute = false;
+    }
+}
 
 //音管理クラス
 public class SoundManager : MonoBehaviour {
 
     protected static SoundManager instance;
+
     public static SoundManager Instance
     {
         get
@@ -13,53 +32,64 @@ public class SoundManager : MonoBehaviour {
             if (instance == null)
             {
                 instance = (SoundManager)FindObjectOfType(typeof(SoundManager));
+
                 if (instance == null)
                 {
                     Debug.LogError("SoundManager Instance Error");
                 }
             }
+
             return instance;
         }
     }
 
-    //音量
+    // 音量
     public SoundVolume volume = new SoundVolume();
 
-    //AudioSource
-    //BGM
+    // === AudioSource ===
+    // BGM
     private AudioSource BGMsource;
-    //SE
+    // SE
     private AudioSource[] SEsources = new AudioSource[16];
-    //音声
+    // 音声
     private AudioSource[] VoiceSources = new AudioSource[16];
 
-    //AudioClip
-    //BGM
+    // === AudioClip ===
+    // BGM
     public AudioClip[] BGM;
-    //SE
+    // SE
     public AudioClip[] SE;
-    //音声
+    // 音声
     public AudioClip[] Voice;
 
-    private void Awake()
+    void Awake()
     {
         GameObject[] obj = GameObject.FindGameObjectsWithTag("SoundManager");
         if (obj.Length > 1)
         {
-            //既に存在しているなら削除
+            // 既に存在しているなら削除
             Destroy(gameObject);
-        } else
+        }
+        else
         {
-            //音管理はシーン遷移では破棄させない
+            // 音管理はシーン遷移では破棄させない
             DontDestroyOnLoad(gameObject);
         }
-        //全てのaudioSouceコンポーネントを追加する
-        //BGM　AudioSouce
+
+        // 全てのAudioSourceコンポーネントを追加する
+
+        // BGM AudioSource
         BGMsource = gameObject.AddComponent<AudioSource>();
-        //BGMはループを有効にする。
+        // BGMはループを有効にする
         BGMsource.loop = true;
 
-        //SE AudioSouce
+        // SE AudioSource
+        for (int i = 0; i < SEsources.Length; i++)
+        {
+            SEsources[i] = gameObject.AddComponent<AudioSource>();
+        }
+
+        // 音声 AudioSource
         for (int i = 0; i < VoiceSources.Length; i++)
         {
             VoiceSources[i] = gameObject.AddComponent<AudioSource>();
@@ -68,7 +98,7 @@ public class SoundManager : MonoBehaviour {
 
     void Update()
     {
-        //ミュート設定
+        // ミュート設定
         BGMsource.mute = volume.Mute;
         foreach (AudioSource source in SEsources)
         {
@@ -79,7 +109,7 @@ public class SoundManager : MonoBehaviour {
             source.mute = volume.Mute;
         }
 
-        //ボリューム設定
+        // ボリューム設定
         BGMsource.volume = volume.BGM;
         foreach (AudioSource source in SEsources)
         {
@@ -91,15 +121,17 @@ public class SoundManager : MonoBehaviour {
         }
     }
 
-    //BGM再生
-    //BGM再生
+
+
+    // ***** BGM再生 *****
+    // BGM再生
     public void PlayBGM(int index)
     {
         if (0 > index || BGM.Length <= index)
         {
             return;
         }
-        //同じBGMの場合は何もしない
+        // 同じBGMの場合は何もしない
         if (BGMsource.clip == BGM[index])
         {
             return;
@@ -109,14 +141,16 @@ public class SoundManager : MonoBehaviour {
         BGMsource.Play();
     }
 
+    // BGM停止
     public void StopBGM()
     {
         BGMsource.Stop();
         BGMsource.clip = null;
     }
 
-    //SE再生
-    //SE再生
+
+    // ***** SE再生 *****
+    // SE再生
     public void PlaySE(int index)
     {
         if (0 > index || SE.Length <= index)
@@ -124,7 +158,7 @@ public class SoundManager : MonoBehaviour {
             return;
         }
 
-        //再生中で無いAudioSouceで鳴らす
+        // 再生中で無いAudioSouceで鳴らす
         foreach (AudioSource source in SEsources)
         {
             if (false == source.isPlaying)
@@ -133,29 +167,30 @@ public class SoundManager : MonoBehaviour {
                 source.Play();
                 return;
             }
-
         }
     }
 
-    //SE停止
+    // SE停止
     public void StopSE()
     {
-        //全てのSE用のAudioSouceを停止する
+        // 全てのSE用のAudioSouceを停止する
         foreach (AudioSource source in SEsources)
         {
             source.Stop();
             source.clip = null;
         }
     }
-    //音声再生
-    //音声再生
+
+
+    // ***** 音声再生 *****
+    // 音声再生
     public void PlayVoice(int index)
     {
         if (0 > index || Voice.Length <= index)
         {
             return;
         }
-        //再生中で無いAudioSourceで鳴らす
+        // 再生中で無いAudioSouceで鳴らす
         foreach (AudioSource source in VoiceSources)
         {
             if (false == source.isPlaying)
@@ -167,10 +202,10 @@ public class SoundManager : MonoBehaviour {
         }
     }
 
-    //音声停止
+    // 音声停止
     public void StopVoice()
     {
-        //全ての音声用のAudioSouceを停止する
+        // 全ての音声用のAudioSouceを停止する
         foreach (AudioSource source in VoiceSources)
         {
             source.Stop();
@@ -178,19 +213,5 @@ public class SoundManager : MonoBehaviour {
         }
     }
 }
-    //音量クラス
-    public class SoundVolume
-    {
-        public float BGM = 1.0f;
-        public float Voice = 1.0f;
-        public float SE = 1.0f;
-        public bool Mute = false;
-        public void Init()
-        {
-            BGM = 1.0f;
-            Voice = 1.0f;
-            SE = 1.0f;
-            Mute = false;
-        }
-    }
+
 
