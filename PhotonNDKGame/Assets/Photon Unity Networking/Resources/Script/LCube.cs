569050ef3de646d9037d2b4b96cba5fc3b3da0ae
doi <a>
2018-02-06 17:13:49 +0900

    Lobby

---
+++ PhotonNDKGame/Assets/Photon Unity Networking/Resources/Script/LCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    float x = 80;
+    float y = 80;
+    void Update()
+    {
+
+       
+        transform.rotation = Quaternion.Euler(x, y, 0);
+
+    }
+}
