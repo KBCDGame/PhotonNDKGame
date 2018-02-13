9249ba024ac1ff6db7e22be19e47c9ce470136d9
NoborioYoshiki <a>
2018-02-14 00:19:41 +0900

    空中コースで落ちた時に車を指定の場所まで戻す処理の追加「

---
+++ PhotonNDKGame/Assets/Photon Unity Networking/Resources/Script/Nobo/SkyRoadCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyRoadCollision : MonoBehaviour {
+
+    [SerializeField]
+    private Transform SkyRoadReturnTrans; //空中コースから落ちた際に車を戻す場所。
+    public void Start()
+    {
+        if (SkyRoadReturnTrans == null)
+        {
+            Debug.Log(this.gameObject.name+"Transformが設定されていません");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (SkyRoadReturnTrans == null)
+        {
+            return;
+        }
+
+        if (other.gameObject.tag == "Car")
+        {
+            //車を戻す処理。
+            other.gameObject.GetComponent<SimpleCarController>().Stop();
+            other.gameObject.GetComponent<SimpleCarController>().transform.SetPositionAndRotation(SkyRoadReturnTrans.position, SkyRoadReturnTrans.rotation);
+        }
+    }
+}
